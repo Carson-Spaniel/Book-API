2ba479333ef4943f507b188b276b25604b51f7ba
Carson-Spaniel <carsonspaniel@gmail.com>
2024-01-20 11:37:03 -0600

    fixing

--- Book_API/urls.py
+++ Book_API/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.BookList.as_view(), name='apiList'),
     path('create/', views.BookCreate.as_view(), name='apiCreate'),
-    path('find/<int:pk>', views.Book.as_view(), name='apiFind'),
-    path('edit/<int:pk>/<int:edit>', views.Book.as_view(), name='apiEdit'),
+    path('find/<int:pk>/', views.Book.as_view(), name='apiFind'),
+    path('edit/<int:pk>/<int:edit>/', views.Book.as_view(), name='apiEdit'),
 ]
